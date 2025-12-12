63604fe7fd614f229aa16e938679b13403206d63
denavigator <asumanlukomwa001@gmail.com>
2025-12-12 13:22:54 +0300

    Add initial settings for MyBaileysBot

---
+++ settings.js
@@ -0,0 +1,1 @@
+{  "botName": "MyBaileysBot",  "prefix": "!",  "owner": "256XXXXXXXXX",  "messages": {    "welcome": "Hello! I am your WhatsApp bot.",    "help": "Commands: !ping !owner !help !menu"  }}
